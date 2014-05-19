cb3aefc6415b1a4f1836844d7b5191d477561586
azarenin@gmail.com <azarenin@gmail.com@bb90f81c-1e10-663d-700f-9058ac00d062>
2014-05-19 04:26:52 +0000

    Split project "90-QCM" into two: "90-QCM_BasicControl" and "91-QCM_Altimetry". The former is the firmware that implements all quad control functions; the latter - augments the former with MPL3115A and US-020 altimetry.

---
+++ trunk/Quad-FW-V1/91-QCM_Altimetry/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=91-QCM_Altimetry.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/91-QCM_Altimetry.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=91-qcmaltimetry.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/91-qcmaltimetry.tar
