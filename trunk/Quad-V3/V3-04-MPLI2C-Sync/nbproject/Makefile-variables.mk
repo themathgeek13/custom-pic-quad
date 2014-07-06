8111e2e7292fecafd3c7449962ccd5f3cd0a9f91
azarenin@gmail.com <azarenin@gmail.com@bb90f81c-1e10-663d-700f-9058ac00d062>
2014-07-06 06:27:06 +0000

    Renaming projects in the renamed directories

--- trunk/Quad-V3/V3-04-MPLI2C-Sync/nbproject/Makefile-variables.mk
+++ trunk/Quad-V3/V3-04-MPLI2C-Sync/nbproject/Makefile-variables.mk
@@ -6,8 +6,8 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=V3-07-MPL.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/V3-07-MPL.production.hex
+CND_ARTIFACT_NAME_default=V3-04-MPLI2C-Sync.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/V3-04-MPLI2C-Sync.production.hex
 CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
-CND_PACKAGE_NAME_default=v3-07-mpl.tar
-CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/v3-07-mpl.tar
+CND_PACKAGE_NAME_default=v3-04-mpli2c-sync.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/v3-04-mpli2c-sync.tar
--- trunk/Quad-V3/V3-06-MPUI2C-Sync/nbproject/Makefile-variables.mk
+++ trunk/Quad-V3/V3-06-MPUI2C-Sync/nbproject/Makefile-variables.mk
@@ -6,8 +6,8 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=V3-04-I2C.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/V3-04-I2C.production.hex
+CND_ARTIFACT_NAME_default=V3-06-MPUI2C-Sync.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/V3-06-MPUI2C-Sync.production.hex
 CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
-CND_PACKAGE_NAME_default=v3-04-i2c.tar
-CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/v3-04-i2c.tar
+CND_PACKAGE_NAME_default=v3-06-mpui2c-sync.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/v3-06-mpui2c-sync.tar
