78821a166f8827ea2a66904d4a6a1a0c8867f5c6
azarenin <azarenin@gmail.com>
2014-07-06 06:27:06 +0000

    Renaming projects in the renamed directories

--- Quad-V3/V3-07-MPUI2C-ASync/nbproject/Makefile-variables.mk
+++ Quad-V3/V3-07-MPUI2C-ASync/nbproject/Makefile-variables.mk
@@ -6,8 +6,8 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=V3-04-I2C.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/V3-04-I2C.production.hex
+CND_ARTIFACT_NAME_default=V3-07-MPUI2C-ASync.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/V3-07-MPUI2C-ASync.production.hex
 CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
-CND_PACKAGE_NAME_default=v3-04-i2c.tar
-CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/v3-04-i2c.tar
+CND_PACKAGE_NAME_default=v3-07-mpui2c-async.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/v3-07-mpui2c-async.tar
--- Quad-V3/V3-07-MPUI2C-ASync/nbproject/Package-default.bash
+++ Quad-V3/V3-07-MPUI2C-ASync/nbproject/Package-default.bash
@@ -10,9 +10,9 @@ CND_CONF=default
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/V3-04-I2C.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=V3-04-I2C.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=v3-04-i2c/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/V3-07-MPUI2C-ASync.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=V3-07-MPUI2C-ASync.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=v3-07-mpui2c-async/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/v3-04-i2c/bin
+makeDirectory ${TMPDIR}/v3-07-mpui2c-async/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/v3-04-i2c.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/v3-07-mpui2c-async.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/v3-04-i2c.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/v3-07-mpui2c-async.tar *
 checkReturnCode
 
 # Cleanup
