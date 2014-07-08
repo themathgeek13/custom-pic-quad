8699dc2f843d9d32c1d5ac0cbcd6a0b372358896
azarenin@gmail.com <azarenin@gmail.com@bb90f81c-1e10-663d-700f-9058ac00d062>
2014-07-08 06:51:14 +0000

    Adding missing file RCSym_Local.h

---
+++ trunk/Quad-FW-V1/00-Modules/RC/RCSym_Local.h
@@ -0,0 +1,52 @@
+#include "RC\RCSym.h"
+#include "RC\RCLocal.h"
+
+//=====================================================
+#ifndef RCSYM_LOCAL_H
+#define	RCSYM_LOCAL_H
+//=====================================================
+
+//************************************************************
+// Definitions used internally for simulation
+//************************************************************
+// RCSymLine Structure is used internally to represent
+// entries in the Sample Array (see RCSymData)
+//-----------------------------------------------------------
+typedef struct
+	{
+	char		Throttle;
+	//----------------------
+	char		Roll;
+	char		Pitch;
+	char		Yaw;
+	} RCSymLine;
+//------------------------------------------------------------
+// Simulation control values
+//------------------------------------------------------------
+extern 	ulong			_RCSymStart;	// Timestamp (in ms.)
+										// of simulation start
+//------------------------------------------------------------
+// Simulation data (defined in the RCSymData file):
+//------------------------------------------------------------
+extern const uint		_RCSymCount;	// Number of entries
+										// in simulation file
+
+extern const RCSymLine	_RCSymStream[];
+
+//************************************************************
+// RCSymReset function - resets simulation sequence
+//************************************************************
+static inline void	RCSymReset()
+	{
+	//--------------------------------------
+	// Reinitialize simulation sequence
+	//--------------------------------------
+	_RCSymStart			= TMRGetRTC();
+	//-----------------------------------------------
+	return;
+	}
+//************************************************************
+
+//=====================================================
+#endif	/* RCSYM_LOCAL_H */
+
