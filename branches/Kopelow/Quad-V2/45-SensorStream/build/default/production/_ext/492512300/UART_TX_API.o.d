7edb4d87592375216606e88d8f03b54938cbf43e
rich.kopelow@gmail.com <rich.kopelow@gmail.com@bb90f81c-1e10-663d-700f-9058ac00d062>
2014-08-23 01:25:32 +0000

    Clean projects

--- branches/Kopelow/Quad-V2/45-SensorStream/build/default/production/_ext/492512300/UART_TX_API.o.d
+++
@@ -1,7 +0,0 @@
-build/default/production/_ext/492512300/UART_TX_API.o:  \
- ../00-Modules/UART/UART_TX_API.c  \
-../00-Modules/UART/UART_TX_Local.h  \
- ../00-Modules/UART/UART_TX.h  \
-../00-Modules/System.h  \
- ../00-Modules/Profile.h  \
-../00-Modules/UART/UART_TX_Profile.h 
